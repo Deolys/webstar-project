faee289e5edd6eecaa446f08182130df8c19c604
Amaimonn <150851609+Amaimonn@users.noreply.github.com>
2024-04-12 22:47:09 +0300

    react

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 import Main from './pages/main';
 import Admin from './pages/admin';
+import Card from './pages/worker-card';
+import Edit from './pages/card-editing';
 
 const App = () => {
   return (
     <>
-      <Main />
+      {/* <Main /> */}
+      {/* <Card /> */}
+      {/* <Edit /> */}
+
     </>
   );
 };
