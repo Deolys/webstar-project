ee1acc3a2bec5bd62aca26777f0cb6e1ea3b9a54
Amaimonn <150851609+Amaimonn@users.noreply.github.com>
2024-04-19 16:38:51 +0300

    app.tsx changed

--- src/app.tsx
+++ src/app.tsx
@@ -3,15 +3,12 @@ import React from 'react';
 import Main from './pages/main';
 import Admin from './pages/admin';
 import Card from './pages/worker-card';
-import Edit from './pages/card-editing';
 
 const App = () => {
   return (
     <>
-      {/* <Main /> */}
-      <Card />
-      {/* <Edit /> */}
-
+      <Main />
+      {/* <Card /> */}
     </>
   );
 };
