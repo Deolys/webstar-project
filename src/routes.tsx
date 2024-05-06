c80d26badfdf5a2ff9d6e2b8fcad8a91f68cbdc4
Deolys <ed03aleks@mail.ru>
2024-05-06 21:19:05 +0300

    error boundary added

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,16 +5,20 @@ import { URLs } from './__data__/urls';
 import Main from './pages/main/main';
 import AdminPanel from './pages/admin-panel/admin';
 import Card from './pages/worker-card/worker-card';
+// import Authentication from './pages/auth/auth';
+import { ErrorBoundary } from './components/error-boundary';
 
 export const PageRoutes = () => (
-  <Routes>
-    <Route path={URLs.baseUrl} element={<Main />} />
-    <Route path={URLs.ui.admin} element={<AdminPanel />} />
-    {/* <Route path={URLs.ui.login} element={< />} />
-    <Route path={URLs.ui.register} element={< />} />
-    <Route path={URLs.ui.favourites} element={<Favourites />} /> */}
-    <Route path={URLs.ui.cardDetail.url} element={<Card />} />
-    <Route path={URLs.ui.cardDetailEdit.url} element={<Card />} />
-    <Route path="*" element={<h1>Page not found</h1>} />
-  </Routes>
+  <ErrorBoundary>
+    <Routes>
+      <Route path={URLs.baseUrl} element={<Main />} />
+      <Route path={URLs.ui.admin} element={<AdminPanel />} />
+      {/* <Route path={URLs.ui.login} element={< />} />
+      <Route path={URLs.ui.register} element={< />} />*/}
+      {/* <Route path={URLs.ui.favourites} element={<Favourites />} /> */}
+      <Route path={URLs.ui.cardDetail.url} element={<Card />} />
+      <Route path={URLs.ui.cardDetailEdit.url} element={<Card />} />
+      <Route path="*" element={<h1>Page not found</h1>} />
+    </Routes>
+  </ErrorBoundary>
 )
