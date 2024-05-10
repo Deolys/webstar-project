bc1b381cbee4ee31f910db978cf67823ceb6f2e8
Deolys <116900245+Deolys@users.noreply.github.com>
2024-05-10 23:27:30 +0300

    routes added (#13)
    * routes added
    
    * error boundary added
    
    * auth route added

---
+++ src/routes.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Routes, Route } from'react-router-dom';
+
+import { URLs } from './__data__/urls';
+import Main from './pages/main/main';
+import AdminPanel from './pages/admin-panel/admin';
+import Card from './pages/worker-card/worker-card';
+import Authentication from './pages/auth/auth';
+import { ErrorBoundary } from './components/error-boundary';
+
+export const PageRoutes = () => (
+  <ErrorBoundary>
+    <Routes>
+      <Route path={URLs.baseUrl} element={<Main />} />
+      <Route path={URLs.ui.admin} element={<AdminPanel />} />
+      <Route path={URLs.ui.login} element={< Authentication/>} />
+      <Route path={URLs.ui.register} element={< Authentication/>} />
+      <Route path={URLs.ui.cardDetail.url} element={<Card />} />
+      <Route path={URLs.ui.cardDetailEdit.url} element={<Card />} />
+      <Route path="*" element={<h1>Page not found</h1>} />
+    </Routes>
+  </ErrorBoundary>
+)
