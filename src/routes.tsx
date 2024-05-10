f708640b1d6682a084fe7504c3794691c571a726
Deolys <ed03aleks@mail.ru>
2024-05-10 23:25:48 +0300

    auth route added

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,7 +5,7 @@ import { URLs } from './__data__/urls';
 import Main from './pages/main/main';
 import AdminPanel from './pages/admin-panel/admin';
 import Card from './pages/worker-card/worker-card';
-// import Authentication from './pages/auth/auth';
+import Authentication from './pages/auth/auth';
 import { ErrorBoundary } from './components/error-boundary';
 
 export const PageRoutes = () => (
@@ -13,9 +13,8 @@ export const PageRoutes = () => (
     <Routes>
       <Route path={URLs.baseUrl} element={<Main />} />
       <Route path={URLs.ui.admin} element={<AdminPanel />} />
-      {/* <Route path={URLs.ui.login} element={< />} />
-      <Route path={URLs.ui.register} element={< />} />*/}
-      {/* <Route path={URLs.ui.favourites} element={<Favourites />} /> */}
+      <Route path={URLs.ui.login} element={< Authentication/>} />
+      <Route path={URLs.ui.register} element={< Authentication/>} />
       <Route path={URLs.ui.cardDetail.url} element={<Card />} />
       <Route path={URLs.ui.cardDetailEdit.url} element={<Card />} />
       <Route path="*" element={<h1>Page not found</h1>} />
